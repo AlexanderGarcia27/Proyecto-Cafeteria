8ad811374182109dbfad24d1362395020cd21c2c
JakoHD <kaj065vwv@gmail.com>
2025-07-09 10:28:39 -0600

    [ADD]Se agrego la segunda pantalla

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { NavbarInicio } from './components/seccion_1/navbarInicio'
 import { TextComponent } from './components/seccion_1/textComponent'
+import { BackgroundSection } from './components/seccion_2/BackgroundSection'
+import { ImageSection } from './components/seccion_2/ImageSection'
+import { TextButtonSection } from './components/seccion_2/TextButtonSection'
 import './index.css'
 
 function App() {
@@ -7,6 +10,14 @@ function App() {
     <>
       <NavbarInicio />
       <TextComponent />
+      <BackgroundSection>
+        <ImageSection src="../public/image_2.jpg" alt="Cafe KL">
+          <div className="section2-image-text">
+            Acompañándote desde 2015.<br />El mejor café en Zacualtipan.
+          </div>
+        </ImageSection>
+        <TextButtonSection />
+      </BackgroundSection>
     </>
   )
 }
