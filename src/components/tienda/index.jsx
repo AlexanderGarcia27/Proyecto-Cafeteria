4eef506d6e48fb9d1e3933cc182e90fe40534ee8
Alexander García <agr270404@gmail.com>
2025-07-24 16:45:01 -0600

    [ADD] Se agregaron carpetas para la tienda

---
+++ src/components/tienda/index.jsx
@@ -0,0 +1,10 @@
+{/* Aqui van a ir los componentes que van a ir en la tienda */}
+function Tienda() {
+  return (
+    <>
+      <div>Hola aqui va todo de la Tienda</div>
+    </>
+  );
+}
+
+export default Tienda;
